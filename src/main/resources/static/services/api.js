9d8ceed3b10650855a764c616d3e483aa37996e3
damhui78 <damhui78@naver.com>
2024-02-24 13:16:40 +0900

    refactoring

--- src/main/resources/static/services/api.js
+++ src/main/resources/static/services/api.js
@@ -1,21 +1,19 @@
-const postApi = async (url, param) => {
+const postApi = (url, param) => {
     console.log('postApi param : ', param);
 
-    const response = await fetch(url, {
+    return fetch(url, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(param)
-    });
-    // .then(response => response.json())
-    // .catch(console.error);
-
-    return await response.json();
+    })
+    .then(response => response.json())
+    .catch(console.error);
 }
 
 const registerUserApi = (user) => {
-    return postApi('/api/users', user);
+    return postApi('/api/users', {user});
 }
 
 const loginApi = (user) => {
